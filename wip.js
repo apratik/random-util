4c0cd22c5694d290295569c275f816326104939d
Abhishek Pratik <pratik.abhishek@gmail.com>
2024-03-01 13:18:54 -0700

    Update wip.js

--- wip.js
+++ wip.js
@@ -38,3 +38,50 @@ module.exports = {
     rename,
     unset
 };
+
+
+
+
+----
+
+
+    // dsl.js
+
+const _ = require("lodash");
+
+function shiftup(payload, from, to) {
+    const value = _.get(payload, from);
+    _.unset(payload, from);
+    _.set(payload, to, { ...value });
+    return payload;
+}
+
+function shiftdown(payload, levelToRemove) {
+    const value = _.get(payload, levelToRemove);
+    _.unset(payload, levelToRemove);
+    _.merge(payload, value); // Merge the contents of the removed level into the payload
+    return payload;
+}
+
+function rename(payload, mappings) {
+    Object.entries(mappings).forEach(([from, to]) => {
+        const value = _.get(payload, from);
+        _.unset(payload, from);
+        _.set(payload, to, value);
+    });
+    return payload;
+}
+
+function unset(payload, fields) {
+    fields.forEach(field => {
+        _.unset(payload, field);
+    });
+    return payload;
+}
+
+module.exports = {
+    shiftup,
+    shiftdown,
+    rename,
+    unset
+};

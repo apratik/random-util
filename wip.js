18f92d86e66fc9fae19216165b6d573d46f3b2cb
Abhishek Pratik <pratik.abhishek@gmail.com>
2024-02-29 17:08:49 -0700

    Update wip.js

--- wip.js
+++ wip.js
@@ -4,14 +4,15 @@ function rename(obj, mapping) {
         const current = _.get(obj, fromPath.join('.'));
         
         if (_.isArray(current)) {
-            current.forEach((value, index) => {
-                const toPath = to.replace(/\[x\]/g, `[${index}]`);
+            const arrayValues = current.map((value, index) => {
+                const toPath = to.replace('[x]', `[${index}]`);
                 if (_.isObject(value)) {
                     rename(value, _.mapKeys(mapping, (value, key) => key.replace(fromPath.join('.'), toPath)));
                 } else {
-                    _.set(obj, toPath, value);
+                    return value;
                 }
             });
+            _.set(obj, to.substring(0, to.lastIndexOf('[x]')), arrayValues);
             _.unset(obj, fromPath.join('.'));
         } else if (_.isObject(current)) {
             _.each(current, (value, key) => {

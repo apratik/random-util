be1d9eb717b8cbad7303fce6101c306637c3f775
Abhishek Pratik <pratik.abhishek@gmail.com>
2024-02-29 17:15:41 -0700

    Update wip.js

--- wip.js
+++ wip.js
@@ -1,10 +1,10 @@
 const jp = require('jsonpath-plus');
 
 function rename(obj, mapping) {
-    Object.entries(mapping).forEach(([from, to]) => {
+    for (const from in mapping) {
+        const to = mapping[from];
         const value = jp.value(obj, from);
-        jp.value(obj, to, value);
-        jp.apply(obj, `$..${from}`, () => jp.value(obj, to));
-    });
+        jp.apply(obj, `$..${to}`, () => value);
+    }
     return obj;
 }

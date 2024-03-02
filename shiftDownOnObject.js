f5d6fd2bcf6ebd204833762c6de2b1fb51ac7f5d
Abhishek Pratik <pratik.abhishek@gmail.com>
2024-03-01 23:33:49 -0700

    Update shiftDownOnObject.js

--- shiftDownOnObject.js
+++ shiftDownOnObject.js
@@ -1,50 +1,50 @@
-const _ = require('lodash');
+const _ = require("lodash");
 
 function shiftDown(obj, parentName) {
-    const payments = obj[parentName]; // Obtain the "payments" object
-    delete obj[parentName]; // Delete the "payments" key
-  
-    for (const [key, value] of Object.entries(payments)) {
-      // Create a unique key if needed (handle potential collisions)
-      let uniqueKey = key;
-      let counter = 1;
-      while (obj.hasOwnProperty(uniqueKey)) {
-        uniqueKey = `${key}_${counter}`;
-        counter++;
-      }
-  
-      obj[uniqueKey] = value; // Assign property to original object
-    }
-  } 
+    _.forOwn(obj, (value, key) => {
+        if (_.isObject(value)) {
+            // Check if the current key matches the parentName
+            if (key === parentName) {
+                // Move the children to the parent's level
+                _.merge(obj, value);
+                delete obj[parentName]; // Delete the parent
+            } else {
+                // Recursively call shiftDown on the child object
+                shiftDown(value, parentName);
+            }
+        }
+    });
+}
+
 // Test object
 let obj = {
-  payments: {
-    debtor: {
-      partyIdentifiers: {
-        id: "123456789",
-        bic: "testbic",
-        date: "2019-01-01"
-      }
+    payments: {
+        debtor: {
+            partyIdentifiers: {
+                id: "123456789",
+                bic: "testbic",
+                date: "2019-01-01"
+            }
+        },
+        other: {
+            partyIdentifiers: {
+                id: "987654321",
+                bic: "testbic",
+                date: "2019-01-02"
+            }
+        }
     },
-    others: {
-      partyIdentifiers: {
-        id: "987654321",
-        bic: "testbic",
-        date: "2019-01-02"
-      }
+    creditor: {
+        partyIdentifiers: {
+            id: "9876xxxx54321",
+            bic: "testbiaac",
+            date: "2019-01xxx-02"
+        }
     }
-  },
-  creditor: {
-    partyIdentifiers: {
-      id: "9876xxxx54321",
-      bic: "testbiaac",
-      date: "2019-01xxx-02"
-    }
-  }
 };
 
 // Call shiftDown function with parent name "payments"
 shiftDown(obj, "payments");
-shiftDown(obj, "creditor");
-console.log(JSON.stringify(obj, null, 2)); // Output the final structured object
-  
+
+// Output the result
+console.log(JSON.stringify(obj, null, 2));

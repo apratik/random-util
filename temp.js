7a6dab6348ff9cdf4c5ed3ed629df9de04c460fe
Abhishek Pratik <pratik.abhishek@gmail.com>
2024-03-07 16:00:36 -0700

    Update temp.js

--- temp.js
+++ temp.js
@@ -237,6 +237,30 @@ module.exports = {
 };
 
 
+function move(payload, mappings) {
+    mappings.forEach(mapping => {
+        const fromPath = Object.keys(mapping)[0];
+        const toPath = mapping[fromPath];
+
+        const sourceValues = jsonpath.query(payload, `$.${fromPath}`);
+
+        if (!sourceValues || !sourceValues.length) return; // Skip if source values are empty or undefined
+
+        if (sourceValues.length === 1 && !Array.isArray(sourceValues[0])) {
+            // If there's only one object in sourceValues and it's not an array, assign it directly
+            jsonpath.value(payload, `$.${toPath}`, sourceValues[0]);
+        } else {
+            // If there are multiple objects or a single array object, assign it as an array
+            jsonpath.value(payload, `$.${toPath}`, sourceValues);
+        }
+
+        // Remove the source path
+        jsonpath.value(payload, `$.${fromPath}`, undefined);
+    });
+
+    return payload;
+}
+
 module.exports = {
     rename,
     unset,

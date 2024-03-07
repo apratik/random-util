83b604702f05554a1c6bd5e5c0de56cca6bd487d
Abhishek Pratik <pratik.abhishek@gmail.com>
2024-03-07 15:42:02 -0700

    Update temp.js

--- temp.js
+++ temp.js
@@ -205,6 +205,40 @@ function removeEmptyFields(obj) {
     });
 }
 
+
+
+
+const jsonpath = require('jsonpath');
+
+function newmove(payload, mappings) {
+    mappings.forEach(mapping => {
+        const fromPath = Object.keys(mapping)[0];
+        const toPath = mapping[fromPath];
+
+        const sourceValues = jsonpath.query(payload, `$.${fromPath}`);
+
+        if (!sourceValues || !sourceValues.length) return; // Skip if source values are empty or undefined
+
+        if (sourceValues.length === 1) {
+            // If there's only one object in sourceValues, assign it directly
+            jsonpath.value(payload, `$.${toPath}`, sourceValues[0]);
+        } else {
+            // If there are multiple objects in sourceValues, assign it as an array
+            jsonpath.value(payload, `$.${toPath}`, sourceValues);
+        }
+
+        // Remove the source path
+        jsonpath.value(payload, `$.${fromPath}`, undefined);
+    });
+
+    return payload;
+}
+
+module.exports = {
+    move,
+};
+
+
 module.exports = {
     removeEmptyFields,
 };
